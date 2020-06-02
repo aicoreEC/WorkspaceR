95b4c3d2964e6ff0e80f60a484c4094dbdf4a35f
aicoreEC <51801164+aicoreEC@users.noreply.github.com>
2020-06-02 19:23:43 +0900

    study ggplot

--- visualization_test.R
+++ visualization_test.R
@@ -11,3 +11,19 @@ Desc(d.pizza[,c("driver","temperature","count","weekday","wine_ordered")], ploti
 
 Desc( c( mtcars$mpg ), plotit = TRUE )
 
+library( ggplot2 )
+
+data_bar = data.frame(xx = 1:10, yy = sample(1:3, 10, replace = T))
+data_bar
+
+ggplot(data=data_bar, aes(x=yy)) + 
+  geom_bar()
+
+ggplot(data=data_bar, aes(x=xx, y=yy)) +
+  geom_bar(stat='identity')
+
+ggplot(data=data_bar, aes(x=xx,y=yy)) + 
+  geom_point(color = '#FFA500') 
+
+ggplot(data=data_, aes(x=xx,y=yy))+ 
+  geom_point(color = '#FFA500') 
